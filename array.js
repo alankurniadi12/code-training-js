a7aae2b399bca49ffdf196c83cacccaaf8e8660a
Alan Kurniadi <alankurniadi53@gmail.com>
2026-03-14 23:03:44 +0700

    Return object in reduce

--- array.js
+++ array.js
@@ -6,7 +6,7 @@ const items = [
     {name: 'Laptop', price: 1000000, quantity: 2},
     {name: 'Playstation', price: 2000000, quantity:0}
 ];
-// Arraw Function for count price all the items
+// Arrow Function for count price all the items
 const sum = items.reduce( function (total, value) {
     // nilai awal total 0, 
     // Kalikan price dengan quantity hasilnya akan disimpan pada total, 
@@ -16,8 +16,35 @@ const sum = items.reduce( function (total, value) {
 }, 0);
 console.log("Jumlah harga barang Rp.",sum)
 
+
+
 //Count item bigger than 70000
 const sumUpSeven = items.reduce((total, value) => {
     return total + (value.price > 700000 ? value.price * value.quantity : 0)
 }, 0)
 console.log("Jumlah harga barang diatas 700000 Rp.",sumUpSeven)
+
+
+
+// Buat fungsi yang mengembalikan object seperti ini:
+// {
+//   totalItems: ?,        // total quantity semua barang
+//   totalPrice: ?,        // total harga semua barang
+//   expensiveItems: ?     // jumlah item dengan price > 700000
+// }
+
+//accumulator valuenya adalah 3 object yang masih nol, setiap iterasi akan mengubah nilai object
+const sumObject = items.reduce((acc, value) => {
+    //ubah nilai objectnya, 
+    acc.totalItems += value.quantity,
+    acc.totalPrice += value.price * value.quantity,
+    acc.expensiveItems += (value.price > 700000 ? +1 :0)
+    return acc
+},{
+    //ini adalah nilai awal dari accumulator
+    totalItems: 0,
+    totalPrice: 0,
+    expensiveItems: 0
+});
+
+console.log("Return Obejct Items=", sumObject)
